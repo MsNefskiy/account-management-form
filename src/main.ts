3496cea6e70e03f9ad26d512d0517eaf25d36f60
MsNefskiy <salionk223457162@gmail.com>
2025-10-23 11:26:40 +0300

    Install and configure PrimeVue, Tailwind CSS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import 'primeicons/primeicons.css';
+import './index.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+    }
+});
 
 app.mount('#app')
